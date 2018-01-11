d398a6da402faac6cd9cf2faa321ed0e7430dbde
Rob Martin <rmartin3@alumni.unca.edu>
2018-01-11 09:54:22 -0500

    Album controller/ model/ view

---
+++ tune/Controllers/AlbumsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tune.Models;
+
+namespace tune.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        // GET: Album/Random
+        public ActionResult Random()
+        {
+            var album = new Album() { Name = "Fleet Foxes" };
+
+            return View(album);
+        }
+    }
+}
---
+++ tune/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tune.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
