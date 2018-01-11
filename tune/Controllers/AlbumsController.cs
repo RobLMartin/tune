364acde37ea92d76befa437dd7574db6e680fece
Rob Martin <rmartin3@alumni.unca.edu>
2018-01-11 14:40:16 -0500

    bootswatch, album and customer controllers/models/views

--- tune/Controllers/AlbumsController.cs
+++ tune/Controllers/AlbumsController.cs
@@ -4,17 +4,62 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using tune.Models;
+using tune.ViewModels;
 
 namespace tune.Controllers
 {
     public class AlbumsController : Controller
     {
-        // GET: Album/Random
-        public ActionResult Random()
+
+        public ViewResult Index()
         {
-            var album = new Album() { Name = "Fleet Foxes" };
+            var albums = GetAlbums();
+
+            return View(albums);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var album = GetAlbums().SingleOrDefault(a => a.Id == id);
+
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
 
             return View(album);
         }
+
+        private IEnumerable<Album> GetAlbums()
+        {
+            return new List<Album>
+            {
+                new Album { Id = 1, Name = "Fleet Foxes - Self Titled" },
+                new Album { Id = 2, Name = "Fleet Foxes - Helplessness Blues" },
+                new Album { Id = 3, Name = "Fleet Foxes - Crack-Up" },
+                new Album { Id = 4, Name = "Radiohead - Moon Shaped Pool" }
+            };
+        }
+
+        //// GET: Album/Random
+        //public ActionResult Random()
+        //{
+        //    var album = new Album() { Name = "Fleet Foxes" };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer { Name = "Rob Martin" },
+        //        new Customer { Name = "Jake Cummings" }
+
+        //    };
+
+        //    var viewModel = new RandomAlbumViewModel
+        //    {
+        //        Album = album,
+        //        Customers = customers
+        //    };
+
+        //    return View(viewModel);
+        //}
+
     }
 }
---
+++ tune/ViewModels/RandomAlbumViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using tune.Models;
+
+namespace tune.ViewModels
+{
+    public class RandomAlbumViewModel
+    {
+        public Album Album { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
